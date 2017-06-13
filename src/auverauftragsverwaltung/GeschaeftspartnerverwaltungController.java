4841b461c25798d36c845dbd62f1df0107f2f9a1
Mudimbi C. S. <>
2017-06-14 00:20:07 +0200

    Geschaeftspartnerverwaltung eingefügt

---
+++ src/auverauftragsverwaltung/GeschaeftspartnerverwaltungController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auverauftragsverwaltung;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mudimbi
+ */
+public class GeschaeftspartnerverwaltungController implements Initializable {
+    
+    public void oeffneGeschaeftspartner(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("Geschaeftspartnerverwaltung.fxml"));
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
