59fa1a01c87c4f7fcd2fbd2a6c3b288231db1bc4
Mudimbi <Mudimbi@mudimbiambasmbp.fritz.box>
2017-06-14 14:14:46 +0200

    - Die Logik zu dem "Abbrechen" Button wurde implementiert

--- src/auverauftragsverwaltung/GeschaeftspartnerverwaltungController.java
+++ src/auverauftragsverwaltung/GeschaeftspartnerverwaltungController.java
@@ -7,7 +7,12 @@ package auverauftragsverwaltung;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -21,8 +26,18 @@ public class GeschaeftspartnerverwaltungController implements Initializable {
     Hier wird die Geschätspartner Scene verwaltet. Der Zugriff auf die Datenbank etc wird hier 
     implementiert
     */
-    
-    
+   @FXML
+    private Button cancelButton;
+   
+   /*
+   Mit dieser Methode schließt man ein offenes/geöffnetes Fenster
+   mit dem "Abbrechen" Button
+   */
+   @FXML
+    public void handleCloseButtonAction(ActionEvent event) {
+    Stage stage = (Stage) cancelButton.getScene().getWindow();
+    stage.close();
+}
     
     
     /**
