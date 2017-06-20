777eb42cf2acf73ef4dae866871bf9cde39f4856
Jakob21667 <j.gettmann@gmx.de>
2017-06-20 17:16:04 +0200

    Anpassungen an den FXML-Files vorgenommen.

--- src/auverauftragsverwaltung/AuVerAuftragsverwaltung.java
+++ src/auverauftragsverwaltung/AuVerAuftragsverwaltung.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package auverauftragsverwaltung;
+
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /*----------------------------------------------------------*/
--- src/auverauftragsverwaltung/StartController.java
+++ src/auverauftragsverwaltung/StartController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package auverauftragsverwaltung;
 
 
@@ -50,7 +45,7 @@ public class StartController implements Initializable {
         Stage stage = new Stage();
         stage.setTitle("Geschäftspartnerverwaltung");
         stage.setScene(scene);
-//        stage.setMaximized(true);
+        stage.setMaximized(true);
         stage.show();
     } catch(IOException e){
         System.out.println("Can't load the Geschaeftspartnerverwaltung!");
