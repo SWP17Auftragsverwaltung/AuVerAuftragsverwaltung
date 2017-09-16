19558b9aecc0c9f3ffe94c976138502c2978a0b2
Jakob21667 <j.gettmann@gmx.de>
2017-09-16 14:25:10 +0200

    Revert "Revert "Revert "AuftragskonditionsDAO erstellt"""
    This reverts commit 9b4943b7e028ea07bd35bbdf01473ee7dea3a25b.

--- src/Datenbank/AuftragskopfDAO.java
+++ src/Datenbank/AuftragskopfDAO.java
@@ -776,7 +776,7 @@ public class AuftragskopfDAO extends DataAccess {
     
     /**
      * Gibt die ID der Geschäftspartner eines Auftrages wieder.
-     * @param auftragskopfID Auftragskopf ID
+     * @param auftragskopfID Geschäftspartner ID
      * @return Typ des Geschäftspartners
      * @throws java.sql.SQLException SQLFehler
      */
@@ -814,7 +814,7 @@ public class AuftragskopfDAO extends DataAccess {
             con.rollback();
         }
         return gpID;
-    }
+    }     
     
-             
+        
 }
